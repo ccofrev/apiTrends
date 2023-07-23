c040f54e524314c3421744ee63f0e67f5b9df632
ccofrev <carlos.cofre@usach.cl>
2023-07-22 23:22:40 -0400

    Se obtienen trenidings de la meta

--- app.js
+++ app.js
@@ -1,100 +1,37 @@
 const express = require('express');
-const axios = require('axios');
 const cheerio = require('cheerio');
+const axios = require('axios');
 
 const app = express();
-const PORT = 3000; // Puedes cambiar el número de puerto según tus preferencias
+const port = 3000;
 
-// Endpoint para obtener información del webscraping
-app.get('/top', async (req, res) => {
-  try {
-    // Realizar una solicitud HTTP al sitio web que deseas hacer webscraping
-    // const url = 'https://getdaytrends.com/chile/';
-    const url = 'https://getdaytrends.com/chile/top/longest/day/';
-    const response = await axios.get(url);
-    const html = response.data;
+// Definir una ruta para el método GET
+app.get('/top', (req, res) => {
+  const url = 'https://www.twitter-trending.com/chile/es'; // Reemplaza esto con la URL del sitio web que deseas scrapear
 
-    // Analizar el HTML utilizando Cheerio para extraer la información deseada
-    const $ = cheerio.load(html);
+  axios.get(url)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
 
-     // Obtener la tabla con la clase deseada
-     const tabla = $(`table.${'ranking', 'top'}`);
- 
-     // Crear un arreglo para almacenar los datos de la tabla
-     const data = [];
- 
-     // Recorrer las filas de la tabla (excepto la primera fila que contiene los encabezados)
-     tabla.find('tr:not(:first-child)').each((index, row) => {
-       const fila = $(row);
- 
-       // Obtener los datos de cada celda de la fila
-       const f1 = fila.find('td:nth-child(1)').text();
-       const Trend = fila.find('td:nth-child(2)').text().substring(1);;
-       const Duracion = fila.find('td:nth-child(3)').text();
-       const Revisado = fila.find('td:nth-child(4)').text();
-       const f5 = fila.find('td:nth-child(5)').text();
- 
- 
-       // Agregar los datos de la fila al arreglo
-    //    data.push({ Trend, Tiempo });
-       data.push({ Trend, Duracion, Revisado });
-     });
- 
-     // Enviar los datos como respuesta en formato JSON
-     res.json(data);
+      // Buscar la etiqueta meta con el nombre "description"
+      const metaDescription = $('meta[name="description"]').attr('content').replace('..','').replace('Tendencias en Chile: ','').split(', ');
 
-  } catch (error) {
-    console.error('Error al obtener datos:', error.message);
-    res.status(500).json({ error: 'Ha ocurrido un error al obtener los datos' });
-  }
+
+      // Verificar si se encontró la etiqueta
+      if (metaDescription) {
+        res.json({ description: metaDescription });
+      } else {
+        res.status(404).json({ error: 'No se encontró la etiqueta meta con nombre "description"' });
+      }
+    })
+    .catch((error) => {
+      console.error('Error al obtener la página:', error);
+      res.status(500).json({ error: 'Error al obtener la página' });
+    });
 });
 
-// Endpoint para obtener información del webscraping
-app.get('/trends', async (req, res) => {
-    try {
-      // Realizar una solicitud HTTP al sitio web que deseas hacer webscraping
-      // const url = 'https://getdaytrends.com/chile/';
-      const url = 'https://getdaytrends.com/chile/top/longest/day/';
-      const response = await axios.get(url);
-      const html = response.data;
-  
-      // Analizar el HTML utilizando Cheerio para extraer la información deseada
-      const $ = cheerio.load(html);
-  
-       // Obtener la tabla con la clase deseada
-       const tabla = $(`table.${'ranking', 'trends'}`);
-   
-       // Crear un arreglo para almacenar los datos de la tabla
-       const data = [];
-   
-       // Recorrer las filas de la tabla (excepto la primera fila que contiene los encabezados)
-       tabla.find('tr:not(:first-child)').each((index, row) => {
-         const fila = $(row);
-   
-         // Obtener los datos de cada celda de la fila
-         const f1 = fila.find('td:nth-child(1)').text();
-         const Trend = fila.find('td:nth-child(2)').text();
-         const Duracion = fila.find('td:nth-child(3)').text();
-         const Revisado = fila.find('td:nth-child(4)').text();
-         const f5 = fila.find('td:nth-child(5)').text();
-   
-   
-         // Agregar los datos de la fila al arreglo
-      //    data.push({ Trend, Tiempo });
-         data.push({ Trend, Duracion });
-       });
-   
-       // Enviar los datos como respuesta en formato JSON
-       res.json(data);
-  
-    } catch (error) {
-      console.error('Error al obtener datos:', error.message);
-      res.status(500).json({ error: 'Ha ocurrido un error al obtener los datos' });
-    }
-  });
-
-
 // Iniciar el servidor
-app.listen(PORT, () => {
-  console.log(`Servidor de webscraping en funcionamiento en http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
