33be0c64adeb002c8fc7e4a0d3c5ff6f4a0778d8
ccofrev <carlos.cofre@usach.cl>
2023-08-08 19:53:14 -0400

    https

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const port = 443;
 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://proyecto-taller-front.vercel.app/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://proyecto-taller-front.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
